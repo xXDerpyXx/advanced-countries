7b50dffecee9542ebbaa4e126590901b7c67d3d5
william341 <wwokwilliam@gmail.com>
2017-12-09 09:09:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var token = "Mzg2Njg4NDE4MjI0Mjc1NDU2.DQTjLg.ZULZRhezhSx_rt0FWieF-VRAW58";
+var token = "meme";
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var call = "!";
