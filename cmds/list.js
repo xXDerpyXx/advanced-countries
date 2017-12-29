1aa437e62534a7deff56882c48e9d44a8ae52df8
zjjpp <jpzen@outlook.com>
2017-12-29 11:15:37 -0800

    command system finished, debug

---
+++ cmds/list.js
@@ -0,0 +1,4 @@
+Command = require("../struct/Command.js");
+module.exports = new Map(
+	["givepeople", new Command("givepeople", "givepeople.js", 2)]
+);
--- modules/commands.js
+++ modules/commands.js
@@ -1,5 +1,23 @@
-Command = require("../struct/Command.js");
-commands = require
-exports.initCommands = () => {
+const Command = require("../struct/Command.js");
+const commands = require("../cmds/list.js");
+const {
+	token,
+	call,
+	width,
+	height,
+	tickSpeed,
+	adminList
+} = require("./config.js");
 
+exports.doCommands = (msg) => {
+	id = msg.author.id;
+	content = msg.content.toLowerCase().split(" ");
+	Object.keys(commands).forEach(function (key) {
+
+		if (call + commands[key].call.toLowerCase() == content[0]) {
+            var cmd = commands[key];
+            const {command} = require(`../cmds/${commands[key].js}`);
+		}
+
+	});
 };
