d943164992c8ceac9393a69f965f6017c9acd662
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-12-30 17:59:55 -0500

    UGHH

--- struct/vars.js
+++ struct/vars.js
@@ -46,7 +46,7 @@ countries      = require("../data/data.json");       // Country Data
 try{
 	map        = require("../data/map.json");        // Map Data
 }catch(err){
-	map        = undefined;                          // Generate if missing
+	map        = generateMapPerlin();                // Generate if missing
 }
 //loadGuns().then(g => guns = g);
 guns           = require("../consts/guns.json");     // Gun Data
