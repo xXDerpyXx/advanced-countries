65d27ba9624989f31d80647ef845a565c66c782d
zjjpp <jpzen@outlook.com>
2017-12-29 20:54:46 -0800

    finsished! review starting.

---
+++ cmds/info/whois.js
@@ -0,0 +1,67 @@
+/*jshint esversion: 6 */
+const {
+	sqlite,
+	path,
+	oneline,
+	Commando,
+	fs,
+	formatMass,
+	save,
+	recursiveWait,
+	getOwnedCells,
+	loadGuns,
+	saveImage,
+	token,
+	call,
+	width,
+	height,
+	tickSpeed,
+	adminList,
+	generateMap,
+	getLocalMap,
+	makeImage,
+	declareWar,
+	government,
+	location,
+	economy,
+	cell,
+	war,
+	gun,
+	countries,
+	map,
+	country
+} = require("../../struct/vars.js");
+
+module.exports = class AlliesCommand extends Commando.Command {
+	constructor(client){
+		super(client, {
+			name: "whois",
+			group: "info",
+			memberName: "whois",
+			description: "Mention a person to see what country they own.",
+			examples: ["!whois @TbK"],
+			args: [
+				{
+					key: "mention",
+					label: "mention",
+					type: "string",
+					default: "idkwhatyouwant"
+				}
+			]
+		});
+	}
+	run(msg, {mention}){
+		id = msg.author.id;
+		c = countries[id];
+		
+		if (msg.mentions.members.first()) {
+			id = msg.mentions.members.first().id, c = countries[id];
+			msg.channel.send(`${msg.mentions.members.first().toString()} is the country of '${c.name}'`);
+		} else {
+			msg.channel.send("Please actually mention someone...");
+		}
+	}
+};
+
+
+
