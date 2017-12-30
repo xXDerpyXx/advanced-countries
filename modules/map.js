a446ee7d94639650a8705a4a42743dd2cf805b96
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-12-30 18:40:24 -0500

    adjusting map

--- modules/map.js
+++ modules/map.js
@@ -64,7 +64,7 @@ exports.generateMapPerlin = () => {
 	
 	for(var x = 0; x < width; x++) {
 		for(var y = 0; y < height; y++) {
-			var temp = simplex.noise2D(x/25,y/25);
+			var temp = simplex.noise2D(x/50,y/50);
 			temp = (temp * 6)+4;
 			/*
 			if(temp < 0){
