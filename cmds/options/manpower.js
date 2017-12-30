8e0e8b41baffafa476d12d60fb56c6e5e56a0032
zjjpp <jpzen@outlook.com>
2017-12-29 19:56:46 -0800

    more progress

---
+++ cmds/options/manpower.js
@@ -0,0 +1,72 @@
+/*jshint esversion: 6 */
+const {
+	sqlite,
+	path,
+	oneline,
+	Commando,
+	fs,
+	formatMass,
+	save,
+	recursiveWait,
+	getOwnedCells,
+	loadGuns,
+	saveImage,
+	token,
+	call,
+	width,
+	height,
+	tickSpeed,
+	adminList,
+	generateMap,
+	getLocalMap,
+	makeImage,
+	declareWar,
+	government,
+	location,
+	economy,
+	cell,
+	war,
+	gun,
+	countries,
+	map,
+	country
+} = require("../../struct/vars.js");
+
+module.exports = class RepowerCommand extends Commando.Command {
+	constructor(client){
+		super(client, {
+			power: "manpower",
+			group: "options",
+			memberpower: "manpower",
+			description: "Change the percentage of your army that's enlisted.",
+			details: oneline`_Very_ strategic.
+			Useful to overpower your enemies or manage your resource cost.
+			Changes resource cost.
+			Changes war ability.`,
+			examples: ["!manpower 100"],
+			args: [
+				{
+					key: "power",
+					prompt: "You gotta choose a percent!",
+					type: "integer"
+				}
+			]
+		});
+	}
+	run(msg, {power}){
+		id = msg.author.id;
+		c = countries[id];
+		if (power <= 100) {
+			if (power >= 0) {
+				countries[id].population.manpower = parseFloat(power / 100);
+				msg.channel.send("Manpower set to " + power + "%");
+				save(countries, map);
+			} else {
+				msg.channel.send("Manpower too low!");
+			}
+		} else {
+			msg.channel.send("Manpower too high!");
+		}
+		
+	}
+};
---
+++ cmds/options/rename.js
@@ -0,0 +1,65 @@
+/*jshint esversion: 6 */
+const {
+	sqlite,
+	path,
+	oneline,
+	Commando,
+	fs,
+	formatMass,
+	save,
+	recursiveWait,
+	getOwnedCells,
+	loadGuns,
+	saveImage,
+	token,
+	call,
+	width,
+	height,
+	tickSpeed,
+	adminList,
+	generateMap,
+	getLocalMap,
+	makeImage,
+	declareWar,
+	government,
+	location,
+	economy,
+	cell,
+	war,
+	gun,
+	countries,
+	map,
+	country
+} = require("../../struct/vars.js");
+
+module.exports = class RenameCommand extends Commando.Command {
+	constructor(client){
+		super(client, {
+			name: "rename",
+			group: "options",
+			memberName: "rename",
+			description: "Rename yourself to something else.",
+			details: oneline`Renames your country.
+			Not really useful, more just confuses your allies and enemies.`,
+			examples: ["!rename hahaha"],
+			args: [
+				{
+					key: "name",
+					prompt: "You gotta have a name!",
+					type: "string"
+				}
+			]
+		});
+	}
+	run(msg, {name}){
+		id = msg.author.id;
+		c = countries[id];
+		if (name.charCodeAt(0) <= 255 && name.charAt(0) != "X" && name.charAt(0) != "*" && name.charAt(0) != "#" && !name.includes("@")) {
+			msg.channel.send("The country of " + countries[id].name + " is now " + name);
+			countries[id].name = name;
+			save(countries, map);
+		} else {
+			msg.channel.send("You need a real name....");
+		}
+	}
+};
