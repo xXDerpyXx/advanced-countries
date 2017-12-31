a613d9733ebee2fd7fae91ca1ce4ffd928af7d81
zjjpp <jpzen@outlook.com>
2017-12-31 10:12:05 -0800

    unsightly but should fix issues furhte on

--- bot.js
+++ bot.js
@@ -25,10 +25,42 @@ client.registry
 	.registerDefaults()
 	.registerCommandsIn(path.join(__dirname, "cmds"));
 
+
+
+var {
+	oneline,
+	formatMass,
+	save,
+	recursiveWait,
+	getOwnedCells,
+	loadGuns,
+	saveImage,
+	token,
+	call,
+	width,
+	height,
+	tickSpeed,
+	adminList,
+	generateMap,
+	generateMapPerlin,
+	getLocalMap,
+	makeImage,
+	declareWar,
+	government,
+	location,
+	economy,
+	cell,
+	war,
+	gun,
+	countries,
+	map,
+	wars,
+	guns
+} = require("./struct/vars.js");
 // ##########################
 //    Functions and Config
 // ##########################
-
+/*
 const {
 	formatMass,
 	save,
@@ -115,9 +147,9 @@ guns["Minigun"].counters = ["AK47","Rocks","Shotgun"];
 guns["Minigun"].modifier = 1.2;
 guns["Minigun"].cost = 15;
 */
-
+/*
 loadGuns().then(g => guns = g);
-
+*/
 try {
 	console.log("[LOADING DATA]");
 	countries = require("./data/data.json");
