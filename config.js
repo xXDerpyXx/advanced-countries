78379b82132b2e1ead6cb2afe553f6439cfe7032
Juan Pablo <jp@tbk-pc.localdomain>
2018-01-01 15:57:11 -0800

    lol

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-	token: "MzkyMDk3NTQ5MzE1NzM1NTcy.DR8QRA.VGTSYLpIOfCu8CKw9kRRXF0IS84",
+	token: "MzkyMDk3NTQ5MzE1NzM1NTcy.DSte1Q.IRVNPUwcTILvwBVuz58XhlUAVe0",
 	call: "!",
 	width: 100,
 	height: 100,
-	tickSpeed: 300000,
+	tickSpeed: 150000,
 	adminList: [
 		"246589957165023232",
 		"338914218470539266",
