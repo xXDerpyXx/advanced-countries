26e59df2a827fc17ab55f443647f8f9acbe3bba2
Juan Pablo <jp@tbk-pc.localdomain>
2018-01-01 22:23:18 -0800

    more stuff!

--- struct/Event.js
+++ struct/Event.js
@@ -1,3 +1,13 @@
 module.exports = class Event{
-    constructor(name,)
-}
+	constructor(name, type, generalChance, minLoyalty = 0,maxLoyalty = 1,minManpower = 0, maxManpower = 1000, requiresWar = false, affectsAllies = false){
+		this.name = name;
+		this.type = type;
+		this.generalChance = generalChance;
+		this.minLoyalty = minLoyalty;
+		this.maxLoyalty = maxLoyalty;
+		this.minManpower = minManpower;
+		this.maxManpower = maxManpower;
+		this.requiresWar = requiresWar;
+		this.affectsAllies = affectsAllies;
+	}
+};  
