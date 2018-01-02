2aee1546e12ab724f7a9df26ed0f9a73ede9beb1
xXDerpyXx <bottledcanofdoom@gmail.com>
2018-01-02 15:48:59 -0500

    adding climate

--- modules/map.js
+++ modules/map.js
@@ -64,8 +64,8 @@ exports.generateMapPerlin = () => {
 
 	for(var x = 0; x < width; x++) {
 		for(var y = 0; y < height; y++) {
-			var temp = simplex.noise2D(x/50,y/50);
-			temp = (temp * 6)+4;
+			var tempElevation = simplex.noise2D(x/50,y/50);
+			tempElevation = (temp * 6)+4;
 			/*
 			if(temp < 0){
 				temp+=5;
@@ -84,8 +84,12 @@ exports.generateMapPerlin = () => {
 				temp = (temp/2)+1;
 			}
 			*/
-			temp += (Math.random()*2)-1;
-			map[x][y].elevation = temp;
+			tempElevation += (Math.random()*2)-1;
+			map[x][y].elevation = tempElevation;
+
+			var tempClimate = simplex.noise2D((x/200)+2000,(y/200)+2000);
+
+			map[x][y].climate = (tempClimate+1)*50;
 
 			/*
 			var temp = simplex.noise2D(x/16,y/16);
