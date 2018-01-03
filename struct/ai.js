536cc8ca3db7fc020940cd5eb583d03d87366701
xXDerpyXx <bottledcanofdoom@gmail.com>
2018-01-02 21:13:22 -0500

    adding ai base

---
+++ struct/ai.js
@@ -0,0 +1,19 @@
+/*
+data contents:
+    map
+
+*/
+var vars = require("./vars.js");
+
+function data(map,countries){
+
+}
+
+function simpleAi(c,data){
+
+}
+
+module.exports = {
+    simpleAi,
+    data
+}
